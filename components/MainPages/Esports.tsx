b2b27f1c0a7d579968471dc155fb2a9bf6be936f
WalidAit69 <aitwalid2000@gmail.com>
2023-11-20 20:38:45 +0100

    enhanced animations

--- components/MainPages/Esports.tsx
+++ components/MainPages/Esports.tsx
@@ -15,11 +15,11 @@ function MainPage() {
   useEffect(() => {
     if (isInview) {
       h1controls.start({ bottom: 0 });
-      controls.start({ y: 0, opacity: 1 });
+      controls.start({ opacity: 1 });
       layercontrols.start({ bottom: "100%" });
     } else {
       h1controls.start({ bottom: "-35px" });
-      controls.start({ y: 0, opacity: 0 });
+      controls.start({ opacity: 0 });
       layercontrols.start({ bottom: 0 });
     }
   }, [isInview]);
@@ -42,16 +42,30 @@ function MainPage() {
           <motion.h1
             animate={h1controls}
             transition={{ duration: 1, delay: 0.6, ease: [0.8, 0, 0, 0.8] }}
-            className="text-4xl font-medium absolute bottom-[-35px]"
+            className="text-4xl font-medium absolute bottom-[-35px] left-0"
           >
-            Mercedes-Benz X Esports.
+            Mercedes-Benz
+          </motion.h1>
+          <motion.h1
+            animate={h1controls}
+            transition={{ duration: 1, delay: 0.7, ease: [0.8, 0, 0, 0.8] }}
+            className="text-4xl font-medium absolute bottom-[-35px] left-[270px]"
+          >
+            X
+          </motion.h1>
+          <motion.h1
+            animate={h1controls}
+            transition={{ duration: 1, delay: 0.8, ease: [0.8, 0, 0, 0.8] }}
+            className="text-4xl font-medium absolute bottom-[-35px] left-[300px]"
+          >
+            Esports.
           </motion.h1>
         </div>
 
         <motion.h3
-          initial={{ y: 20, opacity: 0 }}
+          initial={{ opacity: 0 }}
           animate={controls}
-          transition={{ duration: 1, delay: 0.9, ease: [0.8, 0, 0, 0.8] }}
+          transition={{ duration: 1, delay: 2, ease: [0.8, 0, 0, 0.8] }}
           className="my-4 font-medium text-lg"
         >
           Join the hunt for glory – an interactive League of Legends Esports{" "}
@@ -59,9 +73,9 @@ function MainPage() {
         </motion.h3>
 
         <motion.button
-          initial={{ y: 20, opacity: 0 }}
+          initial={{ opacity: 0 }}
           animate={controls}
-          transition={{ duration: 1, delay: 0.9, ease: [0.8, 0, 0, 0.8] }}
+          transition={{ duration: 1, delay: 2, ease: [0.8, 0, 0, 0.8] }}
           className="text-black bg-white rounded-full flex items-center gap-2 py-[3px] px-3 font-medium text-[14px] hover:text-white hover:bg-[var(--theme-primary)] transition-colors navlinks_transition"
         >
           Discover more <ChevronRight width={15} />
